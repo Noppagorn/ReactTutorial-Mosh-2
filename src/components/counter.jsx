175edd9e738e3a4b09ab44a58f05d7fe9f114225
NoppaGorn <tongnop2540@gmail.com>
2020-08-16 16:46:08 +0700

    can condition on list of tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,29 +2,50 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [], //["tag1", "tag2", "tag3"],
   };
 
   styles = {
     fontWeight: "bold",
     fontSize: 30,
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tage!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  //***********************
+  // true && "HI" will be print HI
+  // true && "HI" && 1 will be print 1
+
   render() {
     return (
-      <React.Fragment>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
-        {/*  <span style={{fontSize: 30}} className="badge badge-primary m-2">
-         */}
-        <span style={this.styles} className={this.getBedgeClasses()}>
+      // <React.Fragment>
+      {
+        /* <img src={this.state.imageUrl} alt="" /> */
+      },
+      {
+        /*  <span style={{fontSize: 30}} className="badge badge-primary m-2">
+         */
+      },
+      {
+        /* <span style={this.styles} className={this.getBedgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+        <button className="btn btn-secondary btn-sm">Increment</button> */
+      },
+      (
+        <div>
+          {this.state.tags.length === 0 && "Please create a new tag"}
+          {this.renderTags()}
+        </div>
+      )
     );
   }
 
