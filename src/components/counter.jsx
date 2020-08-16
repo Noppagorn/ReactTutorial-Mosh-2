ea989e9e363dc60780bbed6f0059e656b3906c15
NoppaGorn <tongnop2540@gmail.com>
2020-08-16 16:31:13 +0700

    can dynamic render

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,13 +15,20 @@ class Counter extends Component {
         {/* <img src={this.state.imageUrl} alt="" /> */}
         {/*  <span style={{fontSize: 30}} className="badge badge-primary m-2">
          */}
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={this.getBedgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+
+  getBedgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count == 0 ? "Zero" : count;
